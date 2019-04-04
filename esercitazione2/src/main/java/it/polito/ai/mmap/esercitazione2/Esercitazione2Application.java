f5d6be39da02a28500a5a0c5d8d5abcb141ff4a3
gjcode <golla.programmazione@gmail.com>
2019-04-04 16:26:51 +0200

    Initial commit

---
+++ esercitazione2/src/main/java/it/polito/ai/mmap/esercitazione2/Esercitazione2Application.java
@@ -0,0 +1,13 @@
+package it.polito.ai.mmap.esercitazione2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Esercitazione2Application.class, args);
+    }
+
+}
