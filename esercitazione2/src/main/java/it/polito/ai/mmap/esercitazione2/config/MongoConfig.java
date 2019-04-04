f44d0572c92d6148e977e50b0f9c39abaecb2bbe
florianMarco <florian.marco@live.com>
2019-04-04 19:03:08 +0200

    MongoConfig - probably okay

--- esercitazione2/src/main/java/it/polito/ai/mmap/esercitazione2/config/MongoConfig.java
+++ esercitazione2/src/main/java/it/polito/ai/mmap/esercitazione2/config/MongoConfig.java
@@ -2,6 +2,8 @@ package it.polito.ai.mmap.esercitazione2.config;
 
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -10,14 +12,21 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class MongoConfig {
 
     @Bean
-    public Mongo mongo() throws Exception {
-        return new Mongo("mongodb+srv://mmap:<password>@esercitazione2-0buq8.mongodb.net/test?retryWrites=true");
+    public MongoClient mongoClient() throws Exception {
+        //mongodb+srv://mmap:mmapmmap1!@esercitazione2-0buq8.mongodb.net/test?retryWrites=true
+        MongoClientURI uri = new MongoClientURI("mongodb+srv://mmap:<password>@esercitazione2-0buq8.mongodb.net/test?retryWrites=true");
+        return new MongoClient(uri);
     }
 
     @Bean
     public MongoTemplate mongoTemplate()
     {
-        return new MongoTemplate(mongo(),"test");
+        try {
+            return new MongoTemplate(mongoClient(),"test");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
