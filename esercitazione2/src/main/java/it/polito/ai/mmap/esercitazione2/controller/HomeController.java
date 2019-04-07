a465989a608caadc238a520deab044c47f6aba42
Angelo <s217167@studenti.polito.it>
2019-04-07 18:34:39 +0200

    Definizione struttura progetto Classe HomeController con dichiarazione vari metodi richiesti

---
+++ esercitazione2/src/main/java/it/polito/ai/mmap/esercitazione2/controller/HomeController.java
@@ -0,0 +1,86 @@
+package it.polito.ai.mmap.esercitazione2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Controller
+public class HomeController {
+
+    // Mapping verso la home dell'applicazione
+    @GetMapping("/")
+    public String home(){
+
+        return "home";
+    }
+
+    //Restituisce una lista JSON con i nomi delle linee presenti nel DBMS
+    @GetMapping("/lines")
+    public String getLines(){
+
+        return "linesJSON";
+    }
+
+    //Restituisce un oggetto JSON contenente due liste, riportanti i dettagli delle fermate di andata e ritorno;
+    @GetMapping("/lines/{nome_linea}")
+    public String getStopsLine(){
+
+        return "stopsJSON";
+    }
+
+    /*
+    * Restituisce un oggetto JSON contenente due liste,riportanti, per ogni fermata di andata e ritorno, l’elenco delle
+    *  persone che devono essere prese in carico o lasciate in corrispondenza della fermata
+    */
+    @GetMapping("/reservation/{nome_linea}/{data}")
+    public String getReservations(){
+
+        return "reservationsJSON";
+    }
+
+    /*
+    * Invia un oggetto JSON contenente il nome dell’alunno da trasportare, l’identificatore della fermata a cui
+    *  sale/scende e il verso di percorrenza (andata/ritorno); restituisce un identificatore univoco della prenotazione
+    *  creata
+    */
+    @PostMapping("/reservation/{nome_linea}/{data}")
+    public String postReservation(){
+
+        return "reservationJSON";
+    }
+
+    /*
+    * Invia un oggetto JSON che permette di aggiornare i dati relativi alla prenotazione indicata
+    *
+    */
+    @PutMapping("/reservation/{nome_linea}/{data}/{reservation_id}")
+    public String updateReservation(){
+
+        return "updatedJSON";
+    }
+
+    /*
+    * Elimina la prenotazione indicata
+    *
+    */
+    @DeleteMapping("/reservation/{nome_linea}/{data}/{reservation_id}")
+    public String deleteReservation(){
+
+        return "deleted";
+    }
+
+    /*
+    * Restituisce la prenotazione
+    */
+    @GetMapping("/reservation/{nome_linea}/{data}/{reservation_id}")
+    public String getReservation(){
+
+        return "reservation";
+    }
+
+
+
+
+}
