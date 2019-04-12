0a9cedc1a70552bec0e8929ce3dd1329948c0dfb
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-04-13 00:00:42 +0200

    Cercato di risolvere il TODO o comunque di fare un Work Advance. Da controllare se si può pulire ancora di più la classe.

--- esercitazione2/src/main/java/it/polito/ai/mmap/esercitazione2/objectDTO/LineaDTO.java
+++ esercitazione2/src/main/java/it/polito/ai/mmap/esercitazione2/objectDTO/LineaDTO.java
@@ -2,12 +2,15 @@ package it.polito.ai.mmap.esercitazione2.objectDTO;
 
 import it.polito.ai.mmap.esercitazione2.entity.FermataEntity;
 import it.polito.ai.mmap.esercitazione2.entity.LineaEntity;
+import it.polito.ai.mmap.esercitazione2.services.MongoService;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @Data
 public class LineaDTO {
@@ -23,14 +26,13 @@ public class LineaDTO {
      * TODO sarebbe bello passargli solo la LineaEntity e poi usare un mongoService per recuperarsi le altre informazioni, ma @Autowired non funziona su oggetti dichairati con new (?)
      * Crea una lineaDTO inserendo tutte le info sulle fermate
      * @param line
-     * @param andata
-     * @param ritorno
+     * @param mongoService
      */
-    public LineaDTO(LineaEntity line, List<FermataEntity> andata,List<FermataEntity> ritorno) {
+    public LineaDTO(LineaEntity line, MongoService mongoService) {
         this.id = line.getId();
         this.nome = line.getNome();
         this.admin = line.getAdmin();
-        this.andata = andata.stream().map(FermataDTO::new).collect(Collectors.toCollection(ArrayList::new));
-        this.ritorno = ritorno.stream().map(FermataDTO::new).collect(Collectors.toCollection(ArrayList::new));
+        this.andata = mongoService.getFermate(line.getAndata()).stream().map(FermataDTO::new).collect(Collectors.toCollection(ArrayList::new));
+        this.ritorno = mongoService.getFermate(line.getAndata()).stream().map(FermataDTO::new).collect(Collectors.toCollection(ArrayList::new));
     }
 }
