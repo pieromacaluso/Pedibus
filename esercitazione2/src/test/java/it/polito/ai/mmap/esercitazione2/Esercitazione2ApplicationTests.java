5a4c065675fe22fd2a29e0225c79829b990d393c
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-05-01 13:57:11 +0200

    Test funzionamento requisiti IMPROVED
    - Pulizia Test
    - Ho migliorato i test suddividendoli in piccoli gruppi.
    - Aggiunto titoli significativi
    - Eliminati @Before e @After: ogni test ripristina lo stato precedente eliminando i dati inseriti nel DB dallo stesso.
    
    Trivial Problemda risolvere:
    - Se un nome utente è già presente nel DB, il test risulterà errato. Una possibile soluzione potrebbe essere quella di inserire stringhe randomiche come nomeAlunno.

--- esercitazione2/src/test/java/it/polito/ai/mmap/esercitazione2/Esercitazione2ApplicationTests.java
+++ esercitazione2/src/test/java/it/polito/ai/mmap/esercitazione2/Esercitazione2ApplicationTests.java
@@ -1,16 +1,276 @@
 package it.polito.ai.mmap.esercitazione2;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.polito.ai.mmap.esercitazione2.model.Prenotazione;
+import it.polito.ai.mmap.esercitazione2.services.JsonHandlerService;
+import it.polito.ai.mmap.esercitazione2.services.MongoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class Esercitazione2ApplicationTests {
 
+    @Autowired
+    JsonHandlerService jsonHandlerService;
+    @Autowired
+    MongoService mongoService;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * GET /lines test
+     *
+     * @throws Exception
+     */
     @Test
-    public void contextLoads() {
+    public void A00_getLines() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        logger.info("Test GET /lines ...");
+        List<String> expectedResult = new ArrayList<>();
+        expectedResult.add("linea1");
+        expectedResult.add("linea2");
+        String expectedJson = mapper.writeValueAsString(expectedResult);
+
+
+        this.mockMvc.perform(get("/lines"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
+
+        logger.info("PASSED");
     }
 
+    /**
+     * GET /lines/linea1 test
+     *
+     * @throws Exception
+     */
+    @Test
+    public void B00_getLine() throws Exception {
+        String linea = "linea1";
+        logger.info("Test GET /lines/" + linea + " ...");
+
+        this.mockMvc.perform(get("/lines/" + linea)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nome").value(linea));
+
+        logger.info("PASSED");
+    }
+
+    @Test
+    public void C00_insertReservation_wrongVerso() throws Exception {
+        Prenotazione res = Prenotazione.builder().nomeAlunno("Piero").idFermata(1).verso(false).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String resJson = mapper.writeValueAsString(res);
+
+        logger.info("Inserimento errato " + res + "...");
+        logger.info("POST /reservations/linea1/2019-01-01/ con verso errato ...");
+
+        this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isInternalServerError());
+
+        logger.info("PASSED");
+
+    }
+
+    @Test
+    public void C01_insertReservation_correctVerso() throws Exception {
+        Prenotazione res = Prenotazione.builder().nomeAlunno("Piero").idFermata(1).verso(true).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String resJson = mapper.writeValueAsString(res);
+
+        logger.info("Inserimento corretto " + res + "...");
+        logger.info("POST /reservations/linea1/2019-01-01/ con verso corretto ...");
+
+        MvcResult result = this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isOk()).andReturn();
+        String idRes = result.getResponse().getContentAsString();
+        logger.info("PASSED");
+
+        logger.info("Ripristino stato precedente...");
+        this.mockMvc.perform(delete("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        logger.info("DONE");
+
+    }
+
+    @Test
+    public void C03_getReservation_check() throws Exception {
+        Prenotazione res = Prenotazione.builder().nomeAlunno("Marco").idFermata(1).verso(true).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String resJson = mapper.writeValueAsString(res);
+
+        logger.info("Inserimento " + res + "...");
+        MvcResult result = this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isOk())
+                .andReturn();
+        String idRes = result.getResponse().getContentAsString();
+        logger.info("Inserito correttamente!");
+
+        logger.info("Controllo reservation " + idRes + " ...");
+        this.mockMvc.perform(get("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nomeAlunno").value(res.getNomeAlunno()))
+                .andExpect(jsonPath("$.idFermata").value(res.getIdFermata()))
+                .andExpect(jsonPath("$.verso").value(res.getVerso()));
+        logger.info("PASSED");
+
+        logger.info("Ripristino stato precedente...");
+        this.mockMvc.perform(delete("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        logger.info("DONE");
+
+    }
+
+    @Test
+    public void C04_insertReservation_duplicate() throws Exception {
+        Prenotazione res = Prenotazione.builder().nomeAlunno("Marco").idFermata(1).verso(true).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String resJson = mapper.writeValueAsString(res);
+        logger.info("Inserimento " + res + "...");
+
+        MvcResult result = this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isOk()).andReturn();
+
+        String idRes = result.getResponse().getContentAsString();
+        logger.info("Inserito correttamente!");
+
+        logger.info("POST /reservations/linea1/2019-01-01/ duplicato ...");
+        this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isInternalServerError());
+        logger.info("PASSED");
+
+        logger.info("Ripristino stato precedente...");
+        this.mockMvc.perform(delete("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        logger.info("DONE");
+    }
+
+    @Test
+    public void C05_getReservation_checkReservationPositionInLine() throws Exception {
+        Prenotazione res = Prenotazione.builder().nomeAlunno("Marco").idFermata(1).verso(true).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String resJson = mapper.writeValueAsString(res);
+        logger.info("Inserimento " + res + "...");
+
+        MvcResult result = this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isOk()).andReturn();
+
+        String idRes = result.getResponse().getContentAsString();
+        logger.info("Inserito correttamente!");
+
+        logger.info("Controllo posizione nomeAlunno nelle linee di " + idRes);
+        logger.info("GET /reservations/linea1/2019-01-01/ per controllo presenza utente ...");
+        this.mockMvc.perform(get("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.alunniPerFermataAndata[0].alunni[0]").value(res.getNomeAlunno()));
+        logger.info("PASSED");
+
+        logger.info("Ripristino stato precedente...");
+        this.mockMvc.perform(delete("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        logger.info("DONE");
+    }
+
+    @Test
+    public void C06_putReservation_updateWrong() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Prenotazione res = Prenotazione.builder().nomeAlunno("Marco").idFermata(1).verso(true).build();
+        String resJson = mapper.writeValueAsString(res);
+        logger.info("Inserimento " + res + "...");
+
+        MvcResult result = this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isOk()).andReturn();
+
+        String idRes = result.getResponse().getContentAsString();
+        logger.info("Inserito correttamente!");
+
+
+        logger.info("Modifico Prenotazione in modo errato");
+        Prenotazione resWrong = Prenotazione.builder().nomeAlunno("Angelo").idFermata(5).verso(true).build();
+        String resWrongJson = mapper.writeValueAsString(resWrong);
+
+        this.mockMvc.perform(put("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON).content(resWrongJson))
+                .andExpect(status().isInternalServerError());
+        logger.info("PASSED");
+
+        logger.info("Ripristino stato precedente...");
+        this.mockMvc.perform(delete("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        logger.info("DONE");
+    }
+
+    @Test
+    public void C07_putReservation_updateCorrect_checkPosition() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Prenotazione res = Prenotazione.builder().nomeAlunno("Marco").idFermata(1).verso(true).build();
+        String resJson = mapper.writeValueAsString(res);
+        logger.info("Inserimento e controllo posizione " + res + "...");
+        MvcResult result = this.mockMvc.perform(post("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON).content(resJson))
+                .andExpect(status().isOk()).andReturn();
+
+        String idRes = result.getResponse().getContentAsString();
+
+        this.mockMvc.perform(get("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.alunniPerFermataAndata[0].alunni[0]").value(res.getNomeAlunno()));
+        logger.info("Inserito e controllato correttamente!");
+
+
+        logger.info("Modifico Prenotazione ...");
+        Prenotazione resCorrect = Prenotazione.builder().nomeAlunno("Angelo").idFermata(5).verso(false).build();
+        String resCorrectJson = mapper.writeValueAsString(resCorrect);
+
+        this.mockMvc.perform(put("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON).content(resCorrectJson))
+                .andExpect(status().isOk());
+
+        logger.info("Controllo nuova posizione prenotazione");
+        this.mockMvc.perform(get("/reservations/linea1/2019-01-01/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.alunniPerFermataRitorno[0].alunni[0]").value(resCorrect.getNomeAlunno()));
+
+        logger.info("PASSED");
+        logger.info("Ripristino stato precedente...");
+        this.mockMvc.perform(delete("/reservations/linea1/2019-01-01/" + idRes)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        logger.info("DONE");
+    }
 }
--- esercitazione2/src/test/java/it/polito/ai/mmap/esercitazione2/repository/UserMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package it.polito.ai.mmap.esercitazione2.repository;
-
-import it.polito.ai.mmap.esercitazione2.bean.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserMongoRepository extends MongoRepository<User, Integer> {
-     User findByName(String name);
-}
