f41655c2ca4fe4ca3a373a4361b5f1066694923b
florianMarco <florian.marco@live.com>
2019-05-02 15:23:29 +0200

    initial esercitazione3 commit

---
+++ esercitazione3/src/main/java/it/polito/ai/mmap/esercitazione3/Esercitazione3Application.java
@@ -0,0 +1,13 @@
+package it.polito.ai.mmap.esercitazione3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Esercitazione3Application.class, args);
+    }
+
+}
---
+++ esercitazione3/src/test/java/it/polito/ai/mmap/esercitazione3/Esercitazione3ApplicationTests.java
@@ -0,0 +1,16 @@
+package it.polito.ai.mmap.esercitazione3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Esercitazione3ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
