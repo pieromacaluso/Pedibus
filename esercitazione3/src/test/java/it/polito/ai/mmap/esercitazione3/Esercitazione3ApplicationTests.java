48543dde97c01aa37d83e666b44d5bc1846d1b7a
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-05-12 23:12:14 +0200

    Aggiunti Test POST /register
    - Register corretto
    - Register duplicate
    - Register non valida

--- esercitazione3/src/test/java/it/polito/ai/mmap/esercitazione3/Esercitazione3ApplicationTests.java
+++ esercitazione3/src/test/java/it/polito/ai/mmap/esercitazione3/Esercitazione3ApplicationTests.java
@@ -1,11 +1,10 @@
 package it.polito.ai.mmap.esercitazione3;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.polito.ai.mmap.esercitazione3.model.Prenotazione;
 import it.polito.ai.mmap.esercitazione3.objectDTO.UserDTO;
+import it.polito.ai.mmap.esercitazione3.repository.UserRepository;
 import it.polito.ai.mmap.esercitazione3.services.JsonHandlerService;
 import it.polito.ai.mmap.esercitazione3.services.MongoService;
-import it.polito.ai.mmap.esercitazione3.services.MongoZonedDateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -17,13 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -39,6 +34,8 @@ public class Esercitazione3ApplicationTests {
     private MockMvc mockMvc;
     @Autowired
     private JavaMailSender mailSender;
+    @Autowired
+    private UserRepository userRepository;
 
 
     @Test
@@ -83,6 +80,86 @@ public class Esercitazione3ApplicationTests {
                 .andExpect(status().isUnauthorized());
 
         logger.info("PASSED");
+    }
+
+    @Test
+    public void postRegister_correct() throws Exception {
+        logger.info("Test POST /register ...");
+        UserDTO user = new UserDTO();
+        user.setEmail("pieromacaluso8@gmail.com");
+        user.setPassword("123456789");
+        user.setPassMatch("123456789");
+        ObjectMapper mapper = new ObjectMapper();
+        String json1 = mapper.writeValueAsString(user);
+
+        this.mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(json1))
+                .andExpect(status().isOk());
+
+        logger.info("PASSED");
+        if (this.userRepository.findByUsername("pieromacaluso8@gmail.com").isPresent()) {
+            this.userRepository.delete(this.userRepository.findByUsername("pieromacaluso8@gmail.com").get());
+        }
+    }
+
+    @Test
+    public void postRegister_duplicate() throws Exception {
+        logger.info("Test POST /register duplicate ...");
+        UserDTO user = new UserDTO();
+        user.setEmail("pieromacaluso8@gmail.com");
+        user.setPassword("123456789");
+        user.setPassMatch("123456789");
+        ObjectMapper mapper = new ObjectMapper();
+        String json1 = mapper.writeValueAsString(user);
+
+        this.mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(json1))
+                .andExpect(status().isOk());
+        this.mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(json1))
+                .andExpect(status().isInternalServerError());
+
+        logger.info("PASSED");
+        if (this.userRepository.findByUsername("pieromacaluso8@gmail.com").isPresent()) {
+            this.userRepository.delete(this.userRepository.findByUsername("pieromacaluso8@gmail.com").get());
+        }
+    }
+
+    @Test
+    public void postRegister_incorrect() throws Exception {
+        logger.info("Test POST /register incorrect ...");
+        UserDTO user = new UserDTO();
+        logger.info("Passwords does not match ...");
+
+        user.setEmail("pieromacaluso8@gmail.com");
+        user.setPassword("123456789");
+        user.setPassMatch("12345678");
+        ObjectMapper mapper = new ObjectMapper();
+        String json1 = mapper.writeValueAsString(user);
+
+        this.mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(json1))
+                .andExpect(status().isInternalServerError());
+
+        logger.info("Password not valid ...");
+
+        user.setEmail("pieromacaluso8@gmail.com");
+        user.setPassword("1");
+        user.setPassMatch("1");
+        String json2 = mapper.writeValueAsString(user);
+
+        this.mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(json2))
+                .andExpect(status().isInternalServerError());
+
+        logger.info("Email not valid ...");
+        user.setEmail("pieromacaluso8gmail.com");
+        user.setPassword("123456789");
+        user.setPassMatch("123456789");
+        String json3 = mapper.writeValueAsString(user);
+
+        this.mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(json3))
+                .andExpect(status().isInternalServerError());
+
+        logger.info("PASSED");
+        if (this.userRepository.findByUsername("pieromacaluso8@gmail.com").isPresent()) {
+            this.userRepository.delete(this.userRepository.findByUsername("pieromacaluso8@gmail.com").get());
+        }
     }
 
 }
