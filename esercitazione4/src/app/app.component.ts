34d9c3097a174da17372c7e96811242d5ed9ecb6
gjcode <golla.programmazione@gmail.com>
2019-05-16 16:50:34 +0200

    initial commit

--- esercitazione4/src/app/app.component.ts
+++ esercitazione4/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Linea} from './linea';
+import {MongoService} from './mongo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'esercitazione4';
+  linee: Linea[] = [];
+
+  constructor(private mongoService: MongoService) {
+    this.linee = mongoService.getLinee();
+  }
+
 }
--- esercitazione4/src/app/app.module.ts
+++ esercitazione4/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatListModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
