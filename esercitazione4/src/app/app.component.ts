24351a5b528f1ea64c03db96208ab6f670cb00c2
gjgits <golla.programmazione@gmail.com>
2019-05-18 17:41:54 +0200

    aggiunto binding presenza alunno

--- esercitazione4/src/app/app.component.ts
+++ esercitazione4/src/app/app.component.ts
@@ -13,10 +13,51 @@ export class AppComponent {
   linee: string[] = [];
   verso: string[] = ['andata', 'ritorno'];
   reservations: Prenotazione;
+  presenze: { fermata: string, alunni: string[] }[] = [];
 
   constructor(private mongoService: MongoService) {
     this.linee = mongoService.getLinee();
     this.reservations = mongoService.getReservation();
   }
+
+  togglePresenza(fermata: string, nomeAlunno: string) {
+    // presenze ancora vuote
+    if (this.presenze.length === 0) {
+      this.presenze.push({
+        fermata, alunni: [nomeAlunno]
+      });
+      return;
+    }
+    let trovoFermata = false;
+    for (const presenza of this.presenze) {
+      if (presenza.fermata === fermata) {
+        trovoFermata = true;
+        if (!this.presente(nomeAlunno)) {
+          presenza.alunni.push(nomeAlunno);
+        } else {
+          const index = presenza.alunni.indexOf(nomeAlunno);
+          presenza.alunni.splice(index, 1);
+        }
+      }
+    }
+    if (!trovoFermata) {
+      this.presenze.push({
+        fermata, alunni: [nomeAlunno]
+      });
+    }
+  }
+
+  presente(nomeAlunno: string): boolean {
+    let trovato = false;
+    for (const presenza of this.presenze) {
+      for (const alunno of presenza.alunni) {
+        if (alunno === nomeAlunno) {
+          trovato = true;
+        }
+      }
+    }
+    return trovato;
+  }
+
 
 }
