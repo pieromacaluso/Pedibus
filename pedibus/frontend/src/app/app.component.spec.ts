233a3470963dbe3e36da5aae08c6be0d9aa150e3
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-06-08 20:35:52 +0200

    Inserimento pedibus/frontend

--- pedibus/frontend/src/app/app.component.spec.ts
+++ pedibus/frontend/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'backend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('frontend');
