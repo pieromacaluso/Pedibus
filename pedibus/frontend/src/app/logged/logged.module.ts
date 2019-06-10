f0f33357555aa73ce33f56b1ee5120d3590085f2
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-06-11 00:44:26 +0200

    fixup! Aggiunta Dialog se si clicca su un bambino non prenotato

--- pedibus/frontend/src/app/logged/logged.module.ts
+++ pedibus/frontend/src/app/logged/logged.module.ts
@@ -15,6 +15,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ToolbarComponent } from './presenze/toolbar/toolbar.component';
 import { ListaPrenotazioniComponent } from './presenze/lista-prenotazioni/lista-prenotazioni.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {AdminBookDialogComponent} from './presenze/lista-prenotazioni/admin-book-dialog/admin-book-dialog.component';
 
 @NgModule({
   declarations: [PresenzeComponent, ToolbarComponent, ListaPrenotazioniComponent, AdminBookDialogComponent],
