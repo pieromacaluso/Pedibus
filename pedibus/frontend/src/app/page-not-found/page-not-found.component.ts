6186311fe2f72e7f424838203773ac35f265cd3d
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-06-14 23:27:36 +0200

    Page Not Found Component Updated

--- pedibus/frontend/src/app/page-not-found/page-not-found.component.ts
+++ pedibus/frontend/src/app/page-not-found/page-not-found.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  error404logo: any = '../../assets/svg/error404.svg';
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  home() {
+    this.router.navigate(['sign-in']);
   }
 
 }
