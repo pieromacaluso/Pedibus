3f071a6209c370f3750425ff8e7870011426e3af
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-06-15 02:17:10 +0200

    Bozza Implementazione Auth Web Socket anche se non penso funzioni

--- pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/configuration/WebSocketConfig.java
+++ pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/configuration/WebSocketConfig.java
@@ -1,10 +1,22 @@
 package it.polito.ai.mmap.pedibus.configuration;
 
+import it.polito.ai.mmap.pedibus.services.JwtTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.Objects;
 
 /**
  * recente -> https://www.baeldung.com/spring-websockets-sendtouser
@@ -13,6 +25,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/messages").setAllowedOrigins("*");
@@ -20,5 +36,22 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/handled","/reservation");
+    }
+
+    // TODO: bozza di pseudo autenticazione WebSocket?
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    Authentication user = jwtTokenService.getAuthentication(Objects.requireNonNull(accessor.getNativeHeader("Auth")).get(0));
+                    accessor.setUser(user);
+                }
+                return message;
+            }
+        });
     }
 }
