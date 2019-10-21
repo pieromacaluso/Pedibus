3a338a88b339e1a1649f081a6ac4e7371edad641
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-10-21 23:15:33 +0200

    [APP] Inizio implementazione Route Disponibilità (dimenticato pomeriggio)

---
+++ pedibus/frontend/src/app/logged/disponibilita/aggiunta-disp/aggiunta-disp.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AggiuntaDispComponent } from './aggiunta-disp.component';
+
+describe('AggiuntaDispComponent', () => {
+  let component: AggiuntaDispComponent;
+  let fixture: ComponentFixture<AggiuntaDispComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AggiuntaDispComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AggiuntaDispComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
