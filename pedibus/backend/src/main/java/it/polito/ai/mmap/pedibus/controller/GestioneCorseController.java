0c4c330cd6a22ae17b4f7cd87eadc508bd308a35
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-10-24 00:04:02 +0200

    [FRONTEND] Piccolezza

--- pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/controller/GestioneCorseController.java
+++ pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/controller/GestioneCorseController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Null;
 import java.util.List;
 
 //TODO implementare la sicurezza degli endpoint
