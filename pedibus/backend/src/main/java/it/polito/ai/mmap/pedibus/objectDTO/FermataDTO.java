1f6e770334e2e8641efc428eba9ee2ab0d3044a2
Piero Macaluso <piero.macaluso@studenti.polito.it>
2019-12-21 01:02:01 +0100

    [BACKEND] Inserite le coordinate nei file JSON delle linee. Entity e DTO modificate per ospitare la posizione. Ho controllato e la deserializzazione avviene correttamente.

--- pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/objectDTO/FermataDTO.java
+++ pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/objectDTO/FermataDTO.java
@@ -6,6 +6,7 @@ import it.polito.ai.mmap.pedibus.services.MongoTimeService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 
 import java.util.Date;
 
@@ -17,6 +18,8 @@ public class FermataDTO {
     private Integer id;
     private String nome;
     private String orario;
+    private GeoJsonPoint location;
+
 
     /**
      * Conversione da Entity a DTO
@@ -27,15 +30,15 @@ public class FermataDTO {
         id = fermataEntity.getId();
         nome = fermataEntity.getName();
         orario = fermataEntity.getOrario();
-    }
-
-    @JsonIgnore
-    public Date getDateOrario()
-    {
-        return MongoTimeService.getMongoZonedDateTimeFromTime(orario);
+        location = fermataEntity.getLocation();
     }
 
     public static int compareTo(FermataDTO fermataDTO, FermataDTO fermataDTO1) {
         return fermataDTO.getId().compareTo(fermataDTO1.getId());
+    }
+
+    @JsonIgnore
+    public Date getDateOrario() {
+        return MongoTimeService.getMongoZonedDateTimeFromTime(orario);
     }
 }
