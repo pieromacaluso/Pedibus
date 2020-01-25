1b582fb17d40ce7f2b9acd4ec2cfbc2c4b707082
florianMarco <florian.marco@live.com>
2020-01-25 18:51:34 +0100

    [Backend] swagger GestioneCorseController

--- pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/controller/GestioneCorseController.java
+++ pedibus/backend/src/main/java/it/polito/ai/mmap/pedibus/controller/GestioneCorseController.java
@@ -1,5 +1,6 @@
 package it.polito.ai.mmap.pedibus.controller;
 
+import io.swagger.annotations.ApiOperation;
 import it.polito.ai.mmap.pedibus.entity.TurnoEntity;
 import it.polito.ai.mmap.pedibus.objectDTO.DispDTO;
 import it.polito.ai.mmap.pedibus.objectDTO.TurnoDTO;
@@ -30,15 +31,17 @@ public class GestioneCorseController {
     /**
      * Permette a una guide/admin di una linea di recuperare la propria disponibilità e e lo stato del relativo turno
      */
+    @ApiOperation("Restituisce la disponibilità del richiedente e lo stato del relativo turno")
     @GetMapping("/disp/{verso}/{data}")
     public DispTurnoResource getDisp(@PathVariable("verso") Boolean verso, @PathVariable("data") String data) throws Exception {
-            return gestioneCorseService.getDispTurnoResource(mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso);
+        return gestioneCorseService.getDispTurnoResource(mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso);
     }
 
     /**
      * Permette a una guide di segnalare la propria disponibilità
      */
     @PostMapping("/disp/{idLinea}/{verso}/{data}")
+    @ApiOperation("Permette di segnalare la propria disponibilità")
     public DispAllResource addDisp(@PathVariable("idLinea") String idLinea, @PathVariable("verso") Boolean verso, @PathVariable("data") String data, @RequestBody Integer idFermata) throws Exception {
         DispTurnoResource dispTurnoResource = gestioneCorseService.addDisp(new DispDTO(idFermata, new TurnoDTO(idLinea, mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso)));
         simpMessagingTemplate.convertAndSend("/dispws-add/" + data + "/" + idLinea + "/" + ((verso) ? 1 : 0), dispTurnoResource.getDisp());
@@ -47,10 +50,12 @@ public class GestioneCorseController {
     }
 
     /**
-     * Permette a una guide di segnalare la propria disponibilità
+     * Permette a una guide di aggiornare la propria disponibilità
      */
     @PostMapping("/disp/{idDisp}")
+    @ApiOperation("Permette di aggiornare una disponibilità")
     public DispAllResource updateDisp(@PathVariable("idDisp") String idDisp, @RequestBody DispAllResource disp) throws Exception {
+        //todo inserire controllo di sicurezza: solo l'admin/guida stessa (?) può cambiare la fermata di una prenotazione (marcof)
         DispTurnoResource res = gestioneCorseService.updateDisp(idDisp, disp);
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         simpMessagingTemplate.convertAndSend("/dispws-up/" + dateFormat.format(res.getTurno().getData()) + "/" + res.getTurno().getIdLinea() + "/" + ((res.getTurno().getVerso()) ? 1 : 0), res.getDisp());
@@ -63,6 +68,7 @@ public class GestioneCorseController {
      * Permette a una guide di annullare la propria disponibilità
      */
     @DeleteMapping("/disp/{idLinea}/{verso}/{data}")
+    @ApiOperation("Permette di annullare una disponibilità")
     public void deleteDisp(@PathVariable("idLinea") String idLinea, @PathVariable("verso") Boolean verso, @PathVariable("data") String data) throws Exception {
         DispAllResource deleted = gestioneCorseService.deleteDisp(new TurnoDTO(idLinea, mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso));
         simpMessagingTemplate.convertAndSend("/dispws/" + deleted.getGuideUsername() + "/" + data + "/" + idLinea + "/" + ((verso) ? 1 : 0), new DispTurnoResource());
@@ -78,6 +84,7 @@ public class GestioneCorseController {
      * @param data
      */
     @GetMapping("/turno/state/{idLinea}/{verso}/{data}")
+    @ApiOperation("Restituisce lo stato di un turno")
     public TurnoResource getTurnoState(@PathVariable("idLinea") String idLinea, @PathVariable("verso") Boolean verso, @PathVariable("data") String data) {
         return gestioneCorseService.getTurnoState(new TurnoDTO(idLinea, mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso));
     }
@@ -91,6 +98,7 @@ public class GestioneCorseController {
      * @param isOpen
      */
     @PutMapping("/turno/state/{idLinea}/{verso}/{data}")
+    @ApiOperation("Permette di chiudere/aprire un turno")
     public void setTurnoState(@PathVariable("idLinea") String idLinea, @PathVariable("verso") Boolean verso, @PathVariable("data") String data, @RequestBody Boolean isOpen) {
         TurnoEntity turno = gestioneCorseService.setTurnoState(new TurnoDTO(idLinea, mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso), isOpen);
         TurnoResource tr = new TurnoResource(turno);
@@ -98,7 +106,7 @@ public class GestioneCorseController {
     }
 
     /**
-     * Restituisce una list di disponibilità per quel turno
+     * Restituisce le disponibilità per il turno indicato
      *
      * @param idLinea
      * @param verso
@@ -107,13 +115,14 @@ public class GestioneCorseController {
      * @throws Exception
      */
     @GetMapping("/turno/disp/{idLinea}/{verso}/{data}")
+    @ApiOperation("Restituisce le disponibilità per il turno indicato")
     public TurnoDispResource getAllTurnoDisp(@PathVariable("idLinea") String idLinea, @PathVariable("verso") Boolean verso, @PathVariable("data") String data) throws Exception {
         return gestioneCorseService.getAllTurnoDisp(new TurnoDTO(idLinea, mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso));
     }
 
     /**
      * Permette di specificare quali degli accompagnatori è stato confermato
-     *2909
+     *
      * @param idLinea
      * @param verso
      * @param data
@@ -121,6 +130,7 @@ public class GestioneCorseController {
      * @throws Exception
      */
     @PostMapping("/turno/disp/{idLinea}/{verso}/{data}")
+    @ApiOperation("Permette di specificare quali degli accompagnatori è stato confermato")
     public void setAllTurnoDisp(@PathVariable("idLinea") String idLinea, @PathVariable("verso") Boolean verso, @PathVariable("data") String data, @RequestBody DispAllResource dispResource) throws Exception {
         gestioneCorseService.setAllTurnoDisp(new TurnoDTO(idLinea, mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso), dispResource);
         DispStateResource state = new DispStateResource(dispResource);
@@ -136,9 +146,8 @@ public class GestioneCorseController {
      * @param data
      */
     @PostMapping("/turno/disp/ack/{idLinea}/{verso}/{data}")
+    @ApiOperation("Permette alle guide confermate di ack la loro conferma")
     public void ackDisp(@PathVariable("idLinea") String idLinea, @PathVariable("verso") Boolean verso, @PathVariable("data") String data) {
         gestioneCorseService.ackDisp(new TurnoDTO(idLinea, mongoTimeService.getMongoZonedDateTimeFromDate(data, true), verso));
     }
-
-
 }
