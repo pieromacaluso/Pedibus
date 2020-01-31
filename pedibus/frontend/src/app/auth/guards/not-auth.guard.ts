c981f0adcbb6fb5a5941c1a36aec43caaf6be68b
Piero Macaluso <piero.macaluso@studenti.polito.it>
2020-01-31 12:48:07 +0100

    [FRONTEND] NotGuard dimenticata

---
+++ pedibus/frontend/src/app/auth/guards/not-auth.guard.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotAuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // Check that the user is logged in...
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate([this.auth.getHome()]);
+      return false;
+    }
+    return true;
+
+  }
+
+
+}
